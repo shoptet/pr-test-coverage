ad31e459ecf49b7eaf5ef42e42c46ee419f151a5
Juraj Šimon <118170619+simonjur@users.noreply.github.com>
2026-05-25 10:49:19 +0200

    chore: use esbuild

--- dist/MarkdownTableGenerator.d.ts
+++
@@ -1,7 +0,0 @@
-import { DirectoryNode } from './DirectoryStructure';
-export declare class MarkdownTableGenerator {
-    generateTable(directoryTree: DirectoryNode | null): string;
-    private generateTableRows;
-    private getIndentation;
-    private padName;
-}
